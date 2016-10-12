3e0606a1c0d93006e0d72d5b4ba6f6321e06dc79
LauraXiulan <laurakramer91@gmail.com>
2016-10-12 17:42:14 +0200

    Added callback to country app

--- node-country-app/app.js
+++ node-country-app/app.js
@@ -1,5 +1,12 @@
 var fs = require('fs')
 var country = require(__dirname + '/json-file-reader')
 
-country(process.argv[2])
-
+var name = process.argv[2]
+country(__dirname + '/countries.json', function(jsonData) {  //To call the callback.
+	for(var i = 0; i<jsonData.length; i++) {
+		if(name == jsonData[i].name) {
+			console.log("Country: " + jsonData[i].name)
+			console.log("Top Level Domain: " + jsonData[i].topLevelDomain)
+		}
+	}
+})
--- node-country-app/json-file-reader.js
+++ node-country-app/json-file-reader.js
@@ -1,20 +1,14 @@
 var fs = require('fs')
-var jsonData = ""
 
-function readFile(name) {
-	fs.readFile(__dirname + '/countries.json', 'utf-8', function(err, data) {
-	if(err) {
-		throw err
-	}
-	jsonData = JSON.parse(data)
-
-	for(var i = 0; i<jsonData.length; i++) {
-		if(name == jsonData[i].name) {
-			console.log("Country: " + jsonData[i].name)
-			console.log("Top Level Domain: " + jsonData[i].topLevelDomain)
-		}
-	}
+function readFile(filename, callback) {
+	fs.readFile(filename, 'utf-8', function(err, data) {
+		if(err) {
+			throw err
+		} 
+		var jsonData = JSON.parse(data)
+		callback(jsonData)
 	})
 }
 
 module.exports = readFile
+
