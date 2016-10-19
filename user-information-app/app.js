64be130dbdc5ead390da68b11dd9715b4ff9221b
LauraXiulan <laurakramer91@gmail.com>
2016-10-19 13:23:09 +0200

    Finished search function of user-information app

--- user-information-app/app.js
+++ user-information-app/app.js
@@ -9,53 +9,68 @@ const app = express()
 app.set('view engine', 'pug')
 app.set('views', __dirname + '/views')
 
-// app.get('/index', (req, res) => {
-// 	response.send('Page opened')
-// })
+app.get('/index', (req, res) => {
+	response.send('Page opened')
+})
 
-// app.get('/', (req, res) => {
-// 	fs.readFile(__dirname + '/users.json', (err, data) => {
-// 		if(err) throw err
-// 		let parsedData = JSON.parse(data)
-// 		res.render('index', {data: parsedData})
-// 	})
-// })
 
-// app.get('/search', (req, res) => {
-// 	// fs.readFile(__dirname + '/users.json', (err, data) => {
-// 	// 	if(err) throw err
-// 	// 	let parsedData = JSON.parse(data)
-// 		res.render('search')
-// 	})
+//Show user list
+app.get('/', (req, res) => {
+	fs.readFile(__dirname + '/users.json', (err, data) => {
+		if(err) throw err
+		let parsedData = JSON.parse(data)
+		res.render('index', {data: parsedData})
+		console.log({data: parsedData})
+	})
+})
 
-app.use(bodyParser.json({extended: true}))
+
+
+//Search users
+app.get('/search', (req, res) => {
+		res.render('search')
+})
+
 app.use(bodyParser.urlencoded({extended: false}))
 
-// app.post('/search', (req, res) => {
-// 	console.log(req.body.user.firstname)
+app.post('/search', (req, res) => {
+	let userSearch = req.body.search
+	fs.readFile(__dirname + '/users.json', (err, data) => {
+		if(err) throw err
+		let parsedData = JSON.parse(data)
+		for(let i = 0; i < parsedData.length; i++) {
+			if(userSearch == parsedData[i].firstname || userSearch == parsedData[i].lastname) {
+			let user = []
+			user.push(parsedData[i].firstname, parsedData[i].lastname, parsedData[i].email)
+			res.render('result', {user: user})	
+			}
+		}
+	})	
+})
 
-// 	// fs.readFile(__dirname + '/users.json', (err, data) => {
-// 	// 	if(err) throw err
-// 	// 	let parsedData = JSON.parse(data)
-// 	// 	for(let i = parsedData.length -1; i >= 0; i--) {
-// 	// 		if(req.body.user.firstname == parsedData.firstname || parsedData.lastname) 
-// 	// 		res.send('You searched for: ' + req.body.user.firstname)
-// 	// 	}
-// 	// })	
-// })
 
 //Add users form
-
-
 app.get('/add', (req, res) => {
 	res.render('add-user')
 })
 
 app.post('/add', (req, res) => {
-	console.log(req.body.firstname)
-	console.log(req.body.lastname)
-	console.log(req.body.email)
-	res.send('Post page')
+	console.log(req.body.first)
+	// let newUser = {
+	// 	firstname: req.body.firstname,
+	// 	lastname: req.body.lastname,
+	// 	email: req.body.email
+	// }
+	// fs.readFile(__dirname + '/users.json', (err, data) => {
+	// 	if(err) throw err
+	// 	let parsedData = JSON.parse(data)
+	// 	parsedData.push(newUser)
+	// 	console.log(parsedData)
+	// })
+		// fs.writeFile(__dirname + '/users.json', parsedData, (mistake) => {
+		// 	if(mistake) throw mistake
+		// })
+
 })
 
 app.listen(8000, () => {
