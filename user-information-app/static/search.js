c2595f6b185e9de6b947422ade2dade6b0e5349b
LauraXiulan <laurakramer91@gmail.com>
2016-10-26 18:06:25 +0200

    Added part of the timer

--- user-information-app/static/search.js
+++ user-information-app/static/search.js
@@ -1,15 +1,41 @@
-$(document).ready( () => {
-	$("#user").keyup( () => {
-		function getSearchResult (){
+//Function to set interval for requests, one every 300 milliseconds
+var lastCall = 0
+function timer() {
+	var now = Date.now()
+	console.log
+	if(now - lastCall > 300) {
+		lastCall = now
+		return true
+	} else {
+		lastCall = now
+		return false
+	}
+}
+
+// for (var i = 200000000 - 1; i >= 0; i--) {
+// 	console.log(timer())
+// }
+
+// var x = 12.00.00.000
+// var y = 12.00.00.300
+
+// 	y-x < 300
+// true = return false
+// false = return true
+
+$(document).ready( function() {
+	$("#user").keyup(function() {
+		if(timer()) {
+			console.log("I am true")
 			if($("#user").val() == ""){
-				$("#result").html(" ")
+				$("#result").html(" ") //Empty the results
 			} else {
 				//Every input should sent a push request to the backend to compare with the database
 				var inputData =	{
 					input: $("#user").val()
 				}
 				var searchResult = []
-				$.post("/search", inputData, (res) => {
+				$.post("/search", inputData, function(res) {
 					$("#result").html("")
 					if(res.data.length === 0) {
 						$("#result").html("User Not Found!")
@@ -20,7 +46,6 @@ $(document).ready( () => {
 					}
 				})
 			}
-		}
-		setInterval(getSearchResult, 300)
+		} else {console.log("I am false")}
 	})
 })
